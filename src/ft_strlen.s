49d86c5ae2ab2a11768db88f3fff0d677a6f994f
Sherchryst <sherchryst@icloud.com>
2020-11-26 20:12:32 +0100

    trythis

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -1,13 +1,16 @@
-			section	.text
-			global	_ft_strlen
 
-_ft_strlen:
-			mov		rax, 0
-			jmp		count
-increment:
-			inc		rax
-count:
-			cmp		BYTE [rdi + rax], 0
-			jne		increment
-return:
-			ret
+                global  ft_strlen
+
+                section .text
+ft_strlen:
+                mov     rdx, 0
+_while:
+                mov     cl, [rdi+rdx]
+                cmp     cl, 0
+                je      return
+                inc     rdx
+                jmp     _while
+
+return:         mov     rax, rdx
+                ret
+
