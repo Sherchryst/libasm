85f1da4087b8fefba62366d6645cd9458e5f2cf1
sgah <sgah@42.student.fr>
2021-05-15 21:00:06 +0200

    cleaning_up

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-gcc -m64 test/test_ft_strcpy.c test/test_ft_strlen.c test/test_ft_strcmp.c test/test_ft_strdup.c test/test_ft_write.c test/test_ft_read.c test/test_ft_atoi_base.c test/ref_atoi_base.c test/test_ft_list_push_front.c test/helper_list.c test/ref_list_push_front.c test/test_ft_list_size.c test/ref_list_size.c main.c -L. -lasm -I ./include
+gcc -m64 test/test_ft_strcpy.c test/test_ft_strlen.c test/test_ft_strcmp.c test/test_ft_strdup.c test/test_ft_write.c test/test_ft_read.c test/test_ft_atoi_base.c test/ref_atoi_base.c test/test_ft_list_push_front.c test/helper_list.c test/ref_list_push_front.c test/test_ft_list_size.c test/ref_list_size.c test/main.c -L. -lasm -I ./include
 ./a.out
